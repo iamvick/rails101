9dfb1d4146c5201a5921f4a47f6d6e638cf1e9f2
Vick.Sun <wendong361@163.com>
2016-12-01 01:20:55 +0800

    implement my post&edit,update,destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
     before_filter :authenticate_user!, :only => [:new, :create]
+    before_filter :find_group_post_and_check_permission, :only => [:edit,:update,:destroy]
     def new
         @group = Group.find(params[:group_id])
         @post = Post.new
@@ -15,7 +16,28 @@ class PostsController < ApplicationController
             render :new
         end
     end
+    def edit
+    end
+    def update
+        if @post.update(post_params)
+            redirect_to account_posts_path(@current_user),notice:"Update Success"
+        else
+            render :edit
+        end
+    end
+    def destroy
+        @post.destroy
+        flash[:alert] = "Post deleted"
+        redirect_to account_posts_path(@current_user)
+    end
     private
+    def find_group_post_and_check_permission
+        @group = Group.find(params[:group_id])
+        if current_user != @group.user
+            redirect_to root_path,alert:"You have no permission."
+        end
+        @post = current_user.posts.find(params[:id])
+    end
     def post_params
         params.require(:post).permit(:content)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
     root 'groups#index' 
     namespace :account do
         resources :groups
+        resources :posts
     end
 end
